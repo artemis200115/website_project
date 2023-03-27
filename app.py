2fc8f33111ac83b28586b5e2750c237b82ecf223
tacogguk // <70328015+artemis200115@users.noreply.github.com>
2023-03-27 02:21:25 +0000

    added code for new webpages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 
 IMAGES =[ {
@@ -41,6 +41,15 @@ def hello_jovian():
     return render_template('home.html', 
                            jobs= jobs, img=IMAGES)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  
+  if not job:
+    return "Not Found", 404
+  
+  return render_template('moleculeinfo.html', 
+                         job=job)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -17,3 +17,14 @@ def load_jobs_from_db():
     for row in results_as_dict:
       jobs.append(row)
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"select * FROM molecules WHERE id = {id}")
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
